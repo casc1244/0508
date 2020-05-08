a0a0c6ec15f7b02d2ba3a6b84b68dffe9d094e8b
1356602646 <1356602646@qq.com>
2020-05-08 15:21:30 +0800

    0508.cpp

--- 0508/0508.cpp
+++ 0508/0508.cpp
@@ -1,20 +1,95 @@
-﻿// 0508.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
 
-#include <iostream>
+
+
+std::vector<Point>  mousePoints;
+Point points;
+
+//鼠标响应函数
+void on_mouse(int EVENT, int x, int y, int flags, void* userdata)
+{
+
+	Mat hh;
+	hh = *(Mat*)userdata;
+	Point p(x, y);
+	switch (EVENT)
+	{
+	case EVENT_LBUTTONDOWN:
+	{
+		points.x = x;
+		points.y = y;
+		mousePoints.push_back(points);
+		circle(hh, points, 4, cvScalar(255, 255, 255), -1);
+		imshow("mouseCallback", hh);
+	}
+	break;
+	}
+
+}
+
+int selectPolygon(cv::Mat srcMat, cv::Mat &dstMat)
+{
+
+	vector<vector<Point>> contours;
+	cv::Mat selectMat;
+
+	cv::Mat m = cv::Mat::zeros(srcMat.size(), CV_32F);
+
+	m = 1;
+
+	if (!srcMat.empty()) {
+		srcMat.copyTo(selectMat);
+		srcMat.copyTo(dstMat);
+	}
+	else {
+		std::cout << "failed to read image!:" << std::endl;
+		return -1;
+	}
+
+	namedWindow("mouseCallback");
+	imshow("mouseCallback", selectMat);
+	setMouseCallback("mouseCallback", on_mouse, &selectMat);
+	waitKey(0);
+	destroyAllWindows();
+	//计算roi
+	contours.push_back(mousePoints);
+	if (contours[0].size() < 3) {
+		std::cout << "failed to read image!:" << std::endl;
+		return -1;
+	}
+
+	drawContours(m, contours, 0, Scalar(0), -1);
+
+	m.copyTo(dstMat);
+
+	return 0;
+}
+
+
+int mouseROI()
+{
+	cv::Mat srcMat = imread("D:\\360downloads\\7.png");
+	cv::Mat dstMat;
+
+	selectPolygon(srcMat, dstMat);
+
+	imshow("srcMat", srcMat);
+	imshow("select Area", dstMat);
+	waitKey(0);
+
+	return 0;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	mouseROI();
+
+
+	return 0;
+
+
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
